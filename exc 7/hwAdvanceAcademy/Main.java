62cc66533e236bd8d07fca31675c7177e34eb1eb
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-10 01:30:35 +0200

    added new exc 7 almost done

---
+++ exc 7/hwAdvanceAcademy/Main.java
@@ -0,0 +1,125 @@
+package hwAdvanceAcademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class Main {
+
+    public static ArrayList<Double> gradesStudents = new ArrayList<>();
+    public static ArrayList<Student> students = new ArrayList<Student>();
+
+    public static void main(String[] args) throws IOException {
+
+
+//===============================================================
+//zad 1
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        double [] arrayFiveNumbers = new double [5] ;
+//        for (int i = 0; i < arrayFiveNumbers.length ; i++) {
+//                arrayFiveNumbers[i] = Double.parseDouble(reader.readLine());
+//        }
+//        double sumForAvg = 0 ;
+//        try {
+//            for (int i = 0; i < arrayFiveNumbers.length + 1 ; i++) {
+//                sumForAvg += arrayFiveNumbers[i];
+//            }
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Минавате извън границата на масива");
+//        }
+//        sumForAvg = sumForAvg/arrayFiveNumbers.length;
+//        System.out.println(sumForAvg);
+//==========================================================
+        // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // int arrayLength = Integer.parseInt(reader.readLine());
+
+//        int[] array = new int  [arrayLength] ;
+//        for (int i = 0; i <array.length ; i++) {
+//            array[i] = Integer.parseInt(reader.readLine());
+//        }
+//
+//        bubbleSortingAlgorithm(array);
+//        System.out.println("Sorted array: ");
+//        printArray(array);
+
+        //==============================================================
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How many new students do you like to add");
+        int addNumberOfStudents = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < addNumberOfStudents; i++) {
+            System.out.println("Please enter student number");
+            String numberOfTheStudent = reader.readLine();
+            System.out.println("Enter the name of the student");
+            String nameOfTheStudent = reader.readLine();
+            System.out.println("How much grades would you like to enter");
+            int numberOfEnteredGrades = Integer.parseInt(reader.readLine());
+            if (numberOfEnteredGrades >= 5) {
+                for (int j = 0; j < numberOfEnteredGrades; j++) {
+                    System.out.printf("%nPlease enter grade number : %d %n", j + 1);
+                    gradesStudents.add(Double.parseDouble(reader.readLine()));
+                }
+            }
+            if (numberOfEnteredGrades < 5) {
+                System.out.println("Pleases enter 5 or moore grades!");//fix it
+            }
+            Student newStudent = new Student(numberOfTheStudent, nameOfTheStudent, gradesStudents);
+            students.add(newStudent);
+        }
+
+        System.out.println(students.toString());
+
+        System.out.println("Please enter student number");
+        String searchInput = reader.readLine();
+        findStudentByNumber(searchInput);
+
+    }
+
+    public static void findStudentByNumber(String studentNumber) {
+
+
+        boolean isPresent = false;
+        for (Student student : students) {
+
+            if (student.getStudentNumber().equals(studentNumber)) {
+                isPresent = true;
+                System.out.println("Student" + student.getName() + "have average grade of :" + student.averageGrade());
+                break;
+            }
+
+            if (!isPresent) {
+                System.out.println("No result were found");
+            }
+        }
+    }
+
+
+    static void bubbleSortingAlgorithm(int[] arrayToSort) {
+        //Метод с който се сортира масив от интове
+        int tempForSwap;//Временна променлива за запазване на
+        boolean isNumbersSwapped = false;//променлива за
+        int arrayLength = arrayToSort.length;//Вземаме дължината на масива в променливата от тип инт
+        for (int i = 0; i < arrayLength - 1; i++) {//обхождаме даденият масив елемент по елемент
+            for (int j = 0; j < arrayLength - i - 1; j++) {
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    tempForSwap = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = tempForSwap;
+                    isNumbersSwapped = true;
+                }
+            }
+            if (!isNumbersSwapped)
+                break;
+        }
+    }
+
+    static void printArray(int[] array) {
+        //Метод за принтиране на масив от интове
+        int arrayLength = array.length;//Вземаме дължината на масива в променливата от тип инт
+        //Вземаме и принтираме всеки елемент от масива последователно
+        for (int value : array) System.out.print(value + " ");
+    }
+}
---
+++ exc 7/hwAdvanceAcademy/Student.java
@@ -0,0 +1,59 @@
+package hwAdvanceAcademy;
+
+import java.util.ArrayList;
+
+public class Student {
+
+    private String studentNumber;
+    private String name;
+    private ArrayList<Double> grades = new ArrayList<>();
+
+    public Student(String studentNumber,String name, ArrayList<Double> grades) {
+        setStudentNumber(studentNumber);
+        setName(name);
+        setGrades(grades);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Double> getGrades() {
+        return this.grades;
+    }
+
+    public void setGrades(ArrayList<Double> grades) {
+        this.grades = grades;
+    }
+
+    public String getStudentNumber() {
+        return this.studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    @Override
+    public String toString() {
+        return "name:" + name +
+                "grades:" + grades +
+                "studentNumber:" + studentNumber;
+    }
+
+    public double averageGrade(){
+        double result = 0.00;
+        double gradesSum = 0.00;
+        for (int i = 0; i < getGrades().size() ; i++) {
+           double grades = getGrades().get(i);
+           gradesSum += grades;
+        }
+        result = gradesSum / getGrades().size();
+        return result;
+    }
+
+}
+
