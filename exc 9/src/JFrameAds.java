43fc5be151ddb395fc1763abc2fd1eb73dc4ee45
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-18 00:21:42 +0200

    exc 9 advance academy

--- exc 9/src/JFrameAds.java
+++ exc 9/src/JFrameAds.java
@@ -6,24 +6,61 @@ import java.awt.event.ActionListener;
 
 public class JFrameAds extends JFrame {
 
-    //-------Buttons--------------
-    private JButton jButtonAddNewAds;
-    private JButton jButtonShow;
+    //----------------------
+    private JButton jButtonAdd;
     private JButton jButtonDelete;
-    //---------------
-    //------------TextFields-----------
-    public static JTextField jTxtFldAdTitle;
-    public static JTextField jTxtFldAdMake;
-    public static JTextField jTxtFldAdModel;
-    public static JTextField jTxtFldAdYear;
-    public static JTextField jTxtFldAdPower;
-    public static JTextField jTxtFldAdPrice;
+    //------------------------------
 
-    //----------------------------------
-    public AdvertisementForVehicle() {
-        super("Advertisement");
+    //----------------
+    public static JTextField jTextFieldAdTitle;
+    public static JTextField jTextFieldAdMake;
+    public static JTextField jTextFieldAdModel;
+    public static JTextField jTextFieldAdYear;
+    public static JTextField jTextFieldAdPower;
+    public static JTextField jTextFieldAdPrice;
+    public static JTextField jTextFieldAdDescription;
+
+    public JFrameAds() {
+        super("Advert");
 //----------------
         setLayout(new FlowLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+//-----------------------------
+
+        jTextFieldAdTitle = new JTextField("     ");
+        jTextFieldAdMake = new JTextField("     ");
+        jTextFieldAdModel = new JTextField("     ");
+        jTextFieldAdYear = new JTextField("     ");
+        jTextFieldAdPower = new JTextField("     ");
+        jTextFieldAdPrice = new JTextField("     ");
+        jTextFieldAdDescription = new JTextField("     ");
+//----------------
+
+        jButtonAdd = new JButton("0");
+        jButtonAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Add Button
+
+            }
+        });
+        jButtonDelete = new JButton("1");
+        jButtonDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Delete Advert Button
+
+            }
+        });
+//----------------
+
+        //====================================
+        add(jButtonAdd);
+        add(jButtonDelete);
+
+
+//----------------
+        pack();
     }
+
 }
