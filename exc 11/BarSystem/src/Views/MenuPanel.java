546d39a2824ed65ead45774b268ded0f41c3d7b3
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-26 19:56:40 +0200

    added uml diagram

--- exc 11/BarSystem/src/Views/MenuPanel.java
+++ exc 11/BarSystem/src/Views/MenuPanel.java
@@ -1,10 +1,47 @@
 package Views;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MenuPanel extends JFrame {
+
+    private JButton jButtonAddNewOrder;
+    private JButton jButtonAddExistingOrder;
+    private JButton jButtonShowBill;
 
     public MenuPanel() {
         super("Main menu");
 
+        jButtonAddNewOrder = new JButton("Add new order");
+        jButtonAddNewOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+
+            }
+        });
+        add(jButtonAddNewOrder);
+
+        jButtonAddExistingOrder = new JButton("Add to existing order");
+        jButtonAddExistingOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+
+            }
+        });
+        add(jButtonAddExistingOrder);
+
+        jButtonShowBill = new JButton("Show bill");
+        jButtonShowBill.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+
+            }
+        });
+        add(jButtonShowBill);
 
     }
 //2.  menu panel
--- exc 11/BarSystem/src/Views/TablesPanel.java
+++ exc 11/BarSystem/src/Views/TablesPanel.java
@@ -1,9 +1,26 @@
 package Views;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class TablesPanel extends JFrame {
+
+    private JButton jButtonTable;
+
     public TablesPanel() {
         super("Tables");
+        String buttonName = "1A";
+        jButtonTable = new JButton(buttonName);
+        jButtonTable.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+            }
+        });
+        add(jButtonTable);
     }
+
 
     //Tables
     //3.  tables panel
