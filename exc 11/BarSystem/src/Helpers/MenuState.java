e03b8b17d903dbb440ff33bd7f732ecb8a811b54
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-30 22:10:59 +0300

    added enums for button options

---
+++ exc 11/BarSystem/src/Helpers/MenuState.java
@@ -0,0 +1,18 @@
+package Helpers;
+
+public enum MenuState {
+    //TODO : add new order option jButton ->  1
+    //TODO : add to existing order option jButton ->  2
+    //TODO : show bill option jButton ->  3
+    /**
+     * Option 1 add new order option jButton ->  1
+     * Option 2 add to existing order option jButton ->  2
+     * Option 3 show bill option jButton ->  3
+     */
+
+    OPTION_ADD_NEW_ORDER
+    ,OPTION_ADD_TO_EXISTING_ORDER
+    ,OPTION_SHOW_BILL_FOR_CURRENT_TABLE
+
+
+}
